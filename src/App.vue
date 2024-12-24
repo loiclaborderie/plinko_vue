0b9b99aee1006bc80b43196587b48f7cd64dfa44
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2024-12-24 16:39:01 +0700

    feat: create main App component with Plinko canvas integration

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+import PlinkoCanva from './components/PlinkoCanva.vue'
+</script>
+
+<template>
+  <header>
+    <h1>Plinko</h1>
+  </header>
+
+  <main>
+    <PlinkoCanva />
+  </main>
+</template>
+
+<style>
+h1 {
+  text-align: center;
+  font-weight: 900;
+  font-size: 40px;
+}
+</style>
