2cfb4621928a71108831c7b7fec17ad2ef24fcff
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2025-07-09 19:09:30 +0700

    refactor: Update Plinko game logic and API integration

--- src/classes/plinkoGameManager.ts
+++ src/classes/plinkoGameManager.ts
@@ -1,52 +1,52 @@
 import { useBalanceStore } from '@/stores/balanceStore'
 import { usePlinkoStore } from '@/stores/plinkoStore'
-import { useUserStore } from '@/stores/userStore'
+import axios from 'axios'
+import { computed } from 'vue'
 
 export class PlinkoGameManager {
   async requestGame(amount: number) {
-    const url = 'http://127.0.0.1:8000/api/plinko/bet'
     const plinko = usePlinkoStore()
-    const user = useUserStore()
-    // post on this url with the row and riskSelected, and an amouht of 0
     try {
-      const _headers: Record<string, string> = {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      }
-      if (user.token) {
-        _headers['Authorization'] = `Bearer ${user.token}`
-      }
-      const _body = JSON.stringify({
+      const result= await axios.post('/api/plinko/bet', {
         row: plinko.maxRows,
         risk: plinko.risk,
-        amount: amount,
-      })
-      const dataFetch = new URLSearchParams({
-        row: plinko.maxRows.toString(),
-        risk: plinko.risk,
-        amount: amount.toString(),
-      })
-      console.log(dataFetch)
-      const result = await fetch(url, {
-        method: 'POST',
-        headers: _headers,
-        body: dataFetch,
+        amount
       })
 
-      // Check if the response status indicates an error
-      if (!result.ok) {
-        const errorData = await result.json()
-        throw new Error(`API Error: ${result.status} - ${errorData.message || 'Unknown error'}`)
-      }
-      this.substractAmount(amount)
+      const { drop_point, multiplier, balance } = result.data
 
-      const data: { drop_point: number; multiplier: number; balance: number } = await result.json()
-      console.log(data)
-      let earningResult: number | null = amount * data.multiplier
-      if (amount === 0) {
-        earningResult = null
-        alert(`You won ${data.multiplier}!`)
+      console.log('result', { drop_point, multiplier, balance })
+
+      let startRound = true
+      if(amount > 0){
+        const balanceStore = useBalanceStore()
+        startRound = balanceStore.substractWager(amount)
+        // we check if we can pay the amount due to start
       }
-      plinko.dropBall(data.drop_point, earningResult)
+
+      if(!startRound){
+        return
+      }
+
+      console.log('api result', result)
+      const earningResult = amount > 0 ? amount * multiplier : null
+      const resultWord = computed(() => {
+        if (multiplier === 1) return 'made even'
+        return multiplier > 1 ? 'earned' : 'lost'
+      })
+
+      if (amount > 0) {
+        if (multiplier === 1) {
+          alert('You made even!')
+        } else {
+          const resultAmount = Math.abs(amount * (multiplier - 1))
+          alert(`You ${resultWord.value} ${resultAmount.toFixed(2)}!`)
+        }
+      } else {
+        alert('You played for nothing hehe!')
+      }
+      console.log('we gonna drop ball', {result, earningResult})
+      plinko.dropBall(drop_point, earningResult)
     } catch (e: unknown) {
       if (e instanceof Error) {
         console.error('Request failed:', e.message)
@@ -56,10 +56,4 @@ export class PlinkoGameManager {
     }
   }
 
-  substractAmount(amount: number) {
-    if (amount > 0) {
-      const balanceStore = useBalanceStore()
-      balanceStore.subtract(amount)
-    }
-  }
 }
