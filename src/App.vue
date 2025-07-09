23ea29bbd13e23c23d1790e0ff8640f2005c861e
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2025-07-09 19:06:01 +0700

    refactor: Import AppLayout directly in App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import { onMounted } from 'vue';import { RouterView } from 'vue-router';
 import { useUserStore } from './stores/userStore';
+import AppLayout from './components/AppLayout.vue';
 const userStore = useUserStore()
 onMounted(async()=> {
   await userStore.checkAuthStatus();
