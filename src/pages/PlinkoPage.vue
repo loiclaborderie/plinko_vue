6ea1253b96a89811428be3a374e93c18113f91e1
loic.laborderie <l.laborderie-boulou@tbs-education.org>
2025-07-09 11:13:10 +0700

    feat(plinko): Add PlinkoPage component
    This commit introduces the PlinkoPage component, which will serve as the main page for the Plinko game.

---
+++ src/pages/PlinkoPage.vue
@@ -0,0 +1,32 @@
+<template>
+    {{ userStore.user }}
+  <header>
+    <h1>Plinko</h1>
+  </header>
+
+  <main>
+    <PlinkoCanva />
+    Balance:
+    <span style="font-size: xx-large; font-weight: 900">{{ balanceStore.balance ?? 0 }}</span>
+  </main>
+
+</template>
+
+<script setup lang="ts">
+import PlinkoCanva from '@/components/PlinkoCanva.vue'
+import { useBalanceStore } from '@/stores/balanceStore'
+import { useUserStore } from '@/stores/userStore'
+
+const balanceStore = useBalanceStore()
+const userStore = useUserStore()
+</script>
+
+
+<style scoped>
+h1 {
+  text-align: center;
+  font-weight: 900;
+  font-size: 40px;
+}
+</style>
+
